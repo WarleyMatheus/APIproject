fbfc8f54b4a4a3de5e9ca896cb08fe95c95e9f71
WarleyMatheus <mwarley735@gmail.com>
2024-03-07 17:23:19 -0300

    correções e incrementações

--- src/main/java/br/com/project/api/controller/Controller.java
+++ src/main/java/br/com/project/api/controller/Controller.java
@@ -3,9 +3,11 @@ package br.com.project.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +19,21 @@ public class Controller {
     
     @Autowired
     private repositorio acao;
+
+    @DeleteMapping("/api/{codigo}")
+    public void remover(@PathVariable int codigo){
+        Pessoa obj = selecionarPeloCodigo(codigo);
+        acao.delete(obj);
+    }
     
     @SuppressWarnings("null")
     @PostMapping("/api")
     public Pessoa cadastrar(@RequestBody Pessoa obj){
+        return acao.save(obj);
+    }
+
+    @PutMapping("/api")
+    public Pessoa editar(@RequestBody Pessoa obj){
         return acao.save(obj);
     }
     
